d0c7cd767f2d9fee86b8a8a77f15452119ef7d17
Dmytro90911621 <slavagospody@gmail.com>
2025-08-26 22:31:55 +0900

    HW 4-6.js

---
+++ Home Work/HW 4/HW 4-6/HW 4-6.js
@@ -0,0 +1,10 @@
+function aline(text) {
+document.write(`
+              <ul>
+                 <li>${text}</li>
+                 <li>${text}</li>
+                 <li>${text}</li>
+              </ul>
+               `)
+}
+aline('JavaScript')
