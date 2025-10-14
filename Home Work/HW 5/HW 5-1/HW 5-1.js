912515afa356035b288cad182b1be14d38e2336c
Dmytro90911621 <slavagospody@gmail.com>
2025-10-14 21:38:17 +0900

    HW 5-1.js

---
+++ Home Work/HW 5/HW 5-1/HW 5-1.js
@@ -0,0 +1,8 @@
+//  – створити функцію, яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+function rectangleArea(a, b) {
+    return a * b;
+}
+
+let area = rectangleArea(5, 10);
+console.log("Площа прямокутника:", area);
